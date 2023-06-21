62acea9a6daf718ba1fc6e0f5293b03d7c52621c
agilimam <agilimamalhafizi04@gmail.com>
2023-06-21 10:32:09 +0700

    membuka File dalam mode menulis.dan menunjuk ke sebuah file dengan outfile.open

--- StreamReaderandWriter/StreamReaderandWriter.cpp
+++ StreamReaderandWriter/StreamReaderandWriter.cpp
@@ -6,3 +6,10 @@ using namespace std;
 int main() {
 	string baris;
 
+	//Membuka File dalam mode Menulis.
+	ofstream outfile;
+	//Menununjuk ke sebuah nama file
+	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q'\' untuk keluar" << endl;
+}
